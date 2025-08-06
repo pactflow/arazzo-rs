4a319c5bcc4b8a44bd0528d60734544cef9902dd
Ronald Holshausen <ronald.holshausen@smartbear.com>
2025-08-06 14:10:49 +1000

    feat: Added some conversion functions to Payload

--- arazzo-models/src/payloads.rs
+++ arazzo-models/src/payloads.rs
@@ -11,6 +11,14 @@ pub trait Payload: Debug + Any {
   /// Returns the raw bytes of the payload. Note that in some cases this will return a new copy
   /// of the payload bytes.
   fn as_bytes(&self) -> Bytes;
+
+  /// Returns the payload as a String.
+  fn as_string(&self) -> String;
+
+  /// Returns the payload as a JSON document if it is easily convertable, otherwise returns None.
+  fn as_json(&self) -> Option<Value> {
+    None
+  }
 }
 
 /// Payload stored as a String value
@@ -20,6 +28,10 @@ pub struct StringPayload(pub String);
 impl Payload for StringPayload {
   fn as_bytes(&self) -> Bytes {
     Bytes::from(self.0.clone())
+  }
+
+  fn as_string(&self) -> String {
+    self.0.clone()
   }
 }
 
@@ -31,6 +43,10 @@ impl Payload for EmptyPayload {
   fn as_bytes(&self) -> Bytes {
     Bytes::new()
   }
+
+  fn as_string(&self) -> String {
+    String::new()
+  }
 }
 
 /// Payload stored as a JSON document. Note that this does not mean a JSON payload (that would be
@@ -41,5 +57,13 @@ pub struct JsonPayload(pub Value);
 impl Payload for JsonPayload {
   fn as_bytes(&self) -> Bytes {
     Bytes::from(self.0.to_string())
+  }
+
+  fn as_string(&self) -> String {
+    self.0.to_string()
+  }
+
+  fn as_json(&self) -> Option<Value> {
+    Some(self.0.clone())
   }
 }
